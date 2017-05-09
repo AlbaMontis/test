5d7c1f8525b44bcd31bf5e7f584735f33567d2a7
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-05-09 23:25:04 +0300

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs')
+  },
+  module: {
+	  rules: [
+	    {
+	      test: /\.css$/,
+	      use: [ 'style-loader', 'css-loader' ]
+	    },
+	    {
+	    	test: /\.js$/,
+	    	exclude: /node_modules/,
+	    	loader: "babel-loader" 
+	    }
+	  ]
+  }
+};

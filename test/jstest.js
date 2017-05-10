45f9050bfb56e6c1d4974f643e013cf31a9826cf
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-05-10 12:08:00 +0300

    Tests fix

--- test/jstest.js
+++ test/jstest.js
@@ -2,6 +2,6 @@ describe('getBody', function() {
     var body = document.body;
 
     it('Should not be ampty', function() {
-        expect(dody.innerHTML).toBe('Hello world!!!');
+        expect(body.innerHTML).toBe('Hello world!!!');
     });
 });
